dab027c7d70669a229e1908cae95750433b4133d
SANKASU <sureshbabu.sankarasubbu@hcahealthcare.com>
2020-06-10 17:30:32 -0500

    Made Parsing changes

--- EDTGHE.cs
+++ EDTGHE.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace EDTGHE
 {
@@ -19,23 +20,36 @@ namespace EDTGHE
         {
             log.LogInformation("Checking for Events from GitHub. ");
 
-            //string name = req.Query["name"];
+            string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            // dynamic data = JsonConvert.DeserializeObject(requestBody);
-            var data = JsonConvert.DeserializeObject<Rootobject>(requestBody);
-            //name = name ?? data?.name;
+            
+            List<Repos> RepoList = JsonConvert.DeserializeObject<List<Repos>>(requestBody);
+            Console.WriteLine("\n Total Repo Count : " + RepoList.Count.ToString());
+            foreach (var item in RepoList)
+            {
+                Console.WriteLine("\n Repo ID : " + item.id.ToString());
+                Console.WriteLine("\n Node : " + item.node_id);
+                Console.WriteLine("\n Repo Name : " + item.name);
+                Console.WriteLine("\n Full Name : " + item.full_name);
+                Console.WriteLine("\n Visiblity : " + item.Visiblity);
+                Console.WriteLine("\n URL : " + item.Url);
+                Console.WriteLine("\n ----------------------------");
+
+                /*
+                // Createe Code that can add these details into the Database tables. 
+                */
+            }
+
             log.LogInformation(requestBody);
-            return new OkResult();
-            //return name != null
-            //    ? (ActionResult)new OkObjectResult($"Hello, {name}")
-            //  : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            
+            //return new OkResult();
+            
+            string responseMessage = string.IsNullOrEmpty(name)
+              ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            //string responseMessage = string.IsNullOrEmpty(name)
-            //  ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //: $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            // return new OkObjectResult(responseMessage);
+             return new OkObjectResult(responseMessage);
         }
     }
 }
---
+++ Repos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDTGHE
+{
+    class Repos
+    {
+        public int id { get; set; }
+        public string node_id { get; set; }  
+        public string name { get; set; }
+        public string full_name { get; set; }    
+        public bool Visiblity { get; set; }
+        public string Url { get; set; }
+    }
+}
